126c5234ffc431a17570d900c8d97491be14fdea
root <root@DESKTOP-William-ENVY.localdomain>
2022-11-14 09:32:58 +0100

    add db join author and paper

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -1,4 +1,5 @@
 class Paper < ApplicationRecord
+  has_and_belongs_to_many :author
   validates :title, :venue, presence: true,
             length: { minimum: 1 }
   validates_numericality_of(:year)
